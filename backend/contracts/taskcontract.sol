4561e20c6049f2afac0b834b27948b3bd2e25845
AMALENDU MANOJ <220229@tkmce.ac.in>
2024-02-01 21:19:37 +0530

    Update taskcontract.sol

--- backend/contracts/taskcontract.sol
+++ backend/contracts/taskcontract.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract taskcontract {
   event adddTask( address recipent,uint taskId);
-  event deleteTask(uint taskId,bool isDeleted);
+  event ddeleteTask(uint taskId,bool isDeleted);
   struct Task{
     uint id ;
     string taskTest;
@@ -13,6 +13,32 @@ contract taskcontract {
   mapping(uint256 => address) taskToOwner;
   function addTask(string memory taskname,bool isDeleted) external{
     uint taskId = tasks.length;
-    tasks.push(Task(taskId,taskname,isDeleted))
+    tasks.push(Task(taskId,taskname,isDeleted));
+    taskToOwner[taskId] = msg.sender;
+    emit adddTask(msg.sender, taskId);
   }
+  function getMyTasks() external view returns (Task[] memory){
+    Task[] memory temporary = new Task[](tasks.length);
+    uint counter = 0;
+    for(uint i = 0;i<tasks.length;i++){
+      if(taskToOwner[i] == msg.sender && tasks[i].isDeleted == false){
+          temporary[counter] = tasks[i];
+          counter++;
+      }
+    }
+      Task[] memory result = new Task[](counter);
+      for(uint i = 0;i<counter;i++){
+        result[i] = temporary[i];
+      } 
+      return result;
+    
+  
+  }
+  function deleteTask(uint taskId,bool isDeleted) external{
+    if(taskToOwner[taskId] == msg.sender){
+      tasks[taskId].isDeleted = isDeleted;
+      emit ddeleteTask(taskId, isDeleted);
+    }
+  }
+
 }
